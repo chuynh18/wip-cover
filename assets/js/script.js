d0447378b7215cfb0b81495918fd505266c76a12
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-04-12 19:04:10 -0700

    changing adjectives

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,24 @@ const adjectives = [
    "Team-oriented",
    "Web",
    "JavaScript",
-   "Java"
+   "Java",
+   "Android"
 ];
+
+const svgTarget = document.getElementById("background2");
+const adj = document.getElementById("span1");
+
+function animate() {
+   adj.textContent = adjectives[Math.floor(Math.random() * adjectives.length)];
+   svgTarget.classList.remove("invisible");
+   svgTarget.classList.add("pulse");
+   setTimeout(function() {
+      svgTarget.classList.remove("pulse");
+      svgTarget.classList.add("invisible");
+   }, 3950);
+   setTimeout(function() {
+      animate();
+   }, 4000);
+}
+
+animate();
