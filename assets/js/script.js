d9fda0e48d01758dc4d2b0271b4be9e1f9e4477c
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-04-12 18:52:24 -0700

    first commit - working stub

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const adjectives = [
+   "Curious",
+   "Open-minded",
+   "Adaptable",
+   "Friendly",
+   "Imaginative",
+   "Optimistic",
+   "Resourceful",
+   "Versatile",
+   "Helpful",
+   "Collaborative",
+   "Team-oriented",
+   "Web",
+   "JavaScript",
+   "Java"
+];
