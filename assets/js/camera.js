4bea61e91a164473a59907281d6d0767401c2147
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-04-16 01:23:16 -0700

    added modal for viewing larger versions of camera photos

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -3,9 +3,12 @@
 // camera configuration object
 const cameraConfig = {
    // current number of photos to display in the camera
-   numPhotos: 16,
+   numPhotos: 20,
    currentPhoto: 0,
-   sound: new Audio("assets/other/shutter.webm")
+   sound: new Audio("assets/other/shutter.webm"),
+   currentlyAnimating: false,
+   cameraClicked: false,
+   photoModal: document.getElementById("photo-modal")
 };
 
 // oh dear, pixel pushing (to align thumbnail image inside the camera body)
@@ -20,6 +23,12 @@ function positionThumbnailInCamera() {
 
 // when the camera is clicked, play shutter sound and display next photo
 function takePhoto() {
+   if (cameraConfig.currentlyAnimating) {
+      return;
+   }
+
+   cameraConfig.cameraClicked = true;
+   cameraConfig.currentlyAnimating = true;
    const cameraScreen = document.getElementById("camera-image");
    const cameraBody = document.getElementById("d7500");
 
@@ -31,14 +40,46 @@ function takePhoto() {
    }, 400);
 
    setTimeout(function() {
+      document.getElementById("camera2").classList.add("camera-screen");
       cameraBody.classList.remove("camera-flash");
       cameraScreen.setAttribute("src", `assets/img/${cameraConfig.currentPhoto}s.jpg`);
       
       (cameraConfig.currentPhoto === cameraConfig.numPhotos - 1) ? cameraConfig.currentPhoto = 0 : cameraConfig.currentPhoto++;
+      
+      cameraConfig.currentlyAnimating = false;
    }, 500);
 }
 
+// when the camera rear display is clicked, show larger image if present
+// otherwise, behave as if the camera was clicked
+function showLargerImage() {
+   if (!cameraConfig.cameraClicked) {
+      takePhoto();
+   } else {
+      document.getElementById("large-photo").setAttribute("src", `assets/img/${cameraConfig.currentPhoto - 1}l.jpg`);
+      cameraConfig.photoModal.style.display = "block";
+   }
+}
 
+function hideModal() {
+   cameraConfig.photoModal.style.display = "none";
+}
+
+// the JavaScript below is executed!
+
+// modal event listeners
+document.addEventListener("click", function(event) {
+   if (event.target === cameraConfig.photoModal) {
+      hideModal();
+   }
+});
+
+document.getElementById("close").addEventListener("click", hideModal);
+
+// attach camera event listeners
 document.getElementById("camera1").addEventListener("click", takePhoto);
+document.getElementById("camera2").addEventListener("click", showLargerImage);
+
+// pixel push camera image into position
 positionThumbnailInCamera();
 window.addEventListener("resize", positionThumbnailInCamera);
